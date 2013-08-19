2520e13cec6f5b79f1baa2ec00882a615aa226d2
darkf <lw9k123@gmail.com>
2013-08-19 05:16:26 -0700

    add tests for eval, stub eval, add private from_str for LispValues

--- lisplike.rs
+++ lisplike.rs
@@ -16,23 +16,73 @@ pub enum LispValue {
 	Fn(~[~str], ~LispValue) // args, body
 }
 
+fn from_sexpr(sexpr: &sexpr::Value) -> ~LispValue {
+	match *sexpr {
+		sexpr::List(ref v) => ~List(v.map(|x| *from_sexpr(x))),
+		sexpr::Num(v) => ~Num(v),
+		sexpr::Str(ref v) => ~Str(v.clone()),
+		sexpr::Atom(ref v) => ~Atom(v.clone())
+	}
+}
+
 /// The type of the global symbol table (string to a value mapping).
 type SymbolTable = HashMap<~str, ~LispValue>;
 
 /// Creates a new symbol table and returns it
-fn new_symt() -> SymbolTable {
+pub fn new_symt() -> SymbolTable {
 	HashMap::new()
 }
 
 /// Binds a symbol in the symbol table. Replaces if it already exists.
-fn bind(symt: &mut SymbolTable, name: ~str, value: ~LispValue) {
+pub fn bind(symt: &mut SymbolTable, name: ~str, value: ~LispValue) {
 	symt.insert(name, value);
 }
 
 /// Look up a symbol in the symbol table. Fails if not found.
-fn lookup(symt: &SymbolTable, name: ~str) -> ~LispValue {
+pub fn lookup(symt: &SymbolTable, name: ~str) -> ~LispValue {
 	match symt.find(&name) {
 		Some(v) => v.clone(),
 		None => fail!("couldn't find symbol: %s", name)
+	}
+}
+
+/// Evaluates an s-expression and returns a value.
+pub fn eval(symt: &mut SymbolTable, input: sexpr::Value) -> ~LispValue {
+	match input {
+		sexpr::List(v) => {
+			if(v.len() == 0) {
+				fail!("eval given empty list")
+			}
+
+			// evaluate a list as a function call
+			match v[0] {
+				sexpr::Atom(sym) => {
+					let f = lookup(symt, sym);
+					fail!("stub: eval")
+				}
+				_ => fail!("function calls take an atom"),
+			}
+
+		}
+		_ => from_sexpr(&input) // return non-list values as they are
+	}
+}
+
+#[cfg(test)]
+mod test {
+	use super::*;
+	use sexpr;
+	use sexpr::from_str;
+
+	fn read(input: &str) -> sexpr::Value {
+		from_str(input).unwrap()
+	}
+
+	#[test]
+	fn test_eval() {
+		let mut symt = new_symt();
+		assert_eq!(eval(&mut symt, read("123")), ~Num(123.0));
+		assert_eq!(eval(&mut symt, read("(id 123)")), ~Num(123.0));
+		// should fail: assert_eq!(eval(&mut symt, read("(1 2 3)")), ~List(~[Num(1.0), Num(2.0), Num(3.0)]));
 	}
 }
